2f82155791dd2463a41cee2ec38055993c5b647e
blademeresuico10 <blademeresuico10@gmail.com>
2025-07-19 19:24:32 +0800

    updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,2 @@
 import './bootstrap';
-import { createApp } from 'vue';
-import reservations from './components/reservations.vue';
-
-const app = createApp({});
-app.component('reservations-view', reservations);
-app.mount('#app');
 
