2560ab6ea8fa069df0059a86d8af3057bfe02cba
blademeresuico10 <blademeresuico10@gmail.com>
2025-07-25 03:28:57 +0800

    ...for deployment

--- database/seeders/tableseeder.php
+++ database/seeders/tableseeder.php
@@ -10,27 +10,34 @@ class tableseeder  extends Seeder
 {
     
     public function run(): void
-    {
-        DB::table('tables')->truncate();
-        DB::table('tables')->insert([
-            ['table_number' => 1, 'capacity' => 4],
-            ['table_number' => 2, 'capacity' => 6],
-            ['table_number' => 3, 'capacity' => 2],
-            ['table_number' => 4, 'capacity' => 8],
-            ['table_number' => 5, 'capacity' => 4],
-            ['table_number' => 6, 'capacity' => 2],
-            ['table_number' => 7, 'capacity' => 6],
-            ['table_number' => 8, 'capacity' => 4],
-            ['table_number' => 9, 'capacity' => 8],
-            ['table_number' => 10, 'capacity' => 2],
-            ['table_number' => 11, 'capacity' => 4],
-            ['table_number' => 12, 'capacity' => 6],
-            ['table_number' => 13, 'capacity' => 2],
-            ['table_number' => 14, 'capacity' => 8],
-            ['table_number' => 15, 'capacity' => 4],
-            ['table_number' => 16, 'capacity' => 6],
-            ['table_number' => 17, 'capacity' => 2],
-           
-        ]);
+{
+    $tables = [
+        ['table_number' => 1, 'capacity' => 4],
+        ['table_number' => 2, 'capacity' => 6],
+        ['table_number' => 3, 'capacity' => 2],
+        ['table_number' => 4, 'capacity' => 8],
+        ['table_number' => 5, 'capacity' => 4],
+        ['table_number' => 6, 'capacity' => 2],
+        ['table_number' => 7, 'capacity' => 6],
+        ['table_number' => 8, 'capacity' => 4],
+        ['table_number' => 9, 'capacity' => 8],
+        ['table_number' => 10, 'capacity' => 2],
+        ['table_number' => 11, 'capacity' => 4],
+        ['table_number' => 12, 'capacity' => 6],
+        ['table_number' => 13, 'capacity' => 2],
+        ['table_number' => 14, 'capacity' => 8],
+        ['table_number' => 15, 'capacity' => 4],
+        ['table_number' => 16, 'capacity' => 6],
+        ['table_number' => 17, 'capacity' => 2],
+    ];
+
+    foreach ($tables as $table) {
+        DB::table('tables')->updateOrInsert(
+            ['table_number' => $table['table_number']], 
+            ['capacity' => $table['capacity']]           
+        );
     }
 }
+
+}
+    
