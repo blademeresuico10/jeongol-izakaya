a94c3d3c7a06cab777bc01ea8ac78637e388880f
blademeresuico10 <blademeresuico10@gmail.com>
2025-07-25 03:15:51 +0800

    ...for deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             menuseeder::class,
-            tableSeeder::class,
+            tableseeder::class,
             users::class,
             stock::class
         ]);
