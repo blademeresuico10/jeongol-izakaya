f472cef6c6c165445b1e759728b097b7db8fb51c
blademeresuico10 <blademeresuico10@gmail.com>
2025-07-25 03:10:06 +0800

    ...for deployment

--- database/seeders/menuseeder.php
+++ database/seeders/menuseeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class Menuseeder extends Seeder
+class MenuSeeder extends Seeder
 {
     
     public function run(): void
