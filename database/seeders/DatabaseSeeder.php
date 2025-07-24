6a6c912fc85a3ed1fb42446a6f8f5de3e6dbd7a4
blademeresuico10 <blademeresuico10@gmail.com>
2025-07-25 03:06:15 +0800

    ...for deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,8 +10,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            menuseeder::class,
-            tableseeder::class,
+            Menuseeder::class,
+            TableSeeder::class,
             users::class,
             stock::class
         ]);
--- database/seeders/menuseeder.php
+++ database/seeders/menuseeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class menuseeder extends Seeder
+class Menuseeder extends Seeder
 {
     
     public function run(): void
